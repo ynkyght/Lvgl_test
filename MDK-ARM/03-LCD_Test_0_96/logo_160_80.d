92f06ac7ee64d1a0639696938227ff5777f2fd53
赢政 <ynkyght@outlook.com>
2025-04-17 15:21:48 +0800

    这是一个stm32h7的lvgl移植测试，目前还没有完成

---
+++ MDK-ARM/03-LCD_Test_0_96/logo_160_80.d
@@ -0,0 +1,1 @@
+./03-lcd_test_0_96/logo_160_80.o: ..\Drivers\BSP\ST7735\logo_160_80.c
---
+++ MDK-ARM/03-LCD_Test_0_96/st7735_reg.d
@@ -0,0 +1,2 @@
+./03-lcd_test_0_96/st7735_reg.o: ..\Drivers\BSP\ST7735\st7735_reg.c \
+  ..\Drivers\BSP\ST7735\st7735_reg.h
